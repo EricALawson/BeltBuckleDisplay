7b5a48357c6f46150523956f44581fbd8548d1cf
Eric <eric.alexander.lawson@gmail.com>
2021-08-18 02:05:43 -0700

    Initial commit

---
+++ app/src/main/java/com/example/beltbuckledisplay/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.beltbuckledisplay
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val startButton: Button = findViewById(R.id.startButton)
+
+    }
+}
