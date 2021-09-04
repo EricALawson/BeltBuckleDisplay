1735bd041421fc9d5e8f965fdf70c221f0e83226
Eric <eric.alexander.lawson@gmail.com>
2021-09-03 20:14:17 -0700

    Changed TelevisionScreenView to concrete class, so layout files can find and use it.

--- app/src/main/java/com/example/beltbuckledisplay/TelevisionScreenView.kt
+++ app/src/main/java/com/example/beltbuckledisplay/TelevisionScreenView.kt
@@ -7,7 +7,7 @@ import android.graphics.Paint
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatImageView
 
-abstract class TelevisionScreenView @JvmOverloads constructor(
+class TelevisionScreenView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : AppCompatImageView(context, attrs, defStyleAttr) {
     private var paint = Paint()
